86475363c39b806fde6e6bf46d7d51523fe97f27
HeungJunBag <p2005hihi@gmail.com>
2026-04-20 16:48:54 +0900

    feat: 토큰 기반 파싱으로 혼합   사칙연산 지원 (t9, t10, t11 통과)

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -3,21 +3,21 @@ package com.ll;
 public class Calc {
     public static int run(String expression) {
 
-        if (expression.contains(" - ")) {
-            String[] expressionBits = expression.split(" - ");
+        // 공백으로 토큰 나눔
+        String[] tokens = expression.split(" ");
 
-            int num1 = Integer.parseInt(expressionBits[0]);
-            int num2 = Integer.parseInt(expressionBits[1]);
+        // 연산자와 숫자 순서대로 번걸아 처리
+        int result = Integer.parseInt(tokens[0]);
+        for (int i = 1; i < tokens.length; i += 2) {
+            String operator = tokens[i];
+            int num = Integer.parseInt(tokens[i + 1]);
 
-            return num1 - num2;
+            if (operator.equals("+")) {
+                result += num;
+            } else if (operator.equals("-")) {
+                result -= num;
+            }
         }
-
-        String[] expressionBits = expression.split(" \\+ ");
-        int result = Integer.parseInt(expressionBits[0]);
-        for (int i = 1; i < expressionBits.length; i++) {
-            result += Integer.parseInt(expressionBits[i]);
-        }
-
         return result;
     }
 }
