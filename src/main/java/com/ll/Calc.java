e1bdc189728af6605a6dffaaeca3567e038cef02
HeungJunBag <p2005hihi@gmail.com>
2026-04-20 17:16:02 +0900

    feat: 연산자 우선순위 지원 (* 먼저 계산, t15, t16, t17 통과)

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -1,25 +1,46 @@
 package com.ll;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Calc {
     public static int run(String expression) {
+        List<String> tokens = new ArrayList<>(Arrays.asList(expression.split(" ")));
 
-        // 공백으로 토큰 나눔
-        String[] tokens = expression.split(" ");
+        // 곱셈 계산 처리
+        int i = 0;
+        while (i < tokens.size()) {
+            if (tokens.get(i).equals("*")) {
 
-        // 연산자와 숫자 순서대로 번걸아 처리
-        int result = Integer.parseInt(tokens[0]);
-        for (int i = 1; i < tokens.length; i += 2) {
-            String operator = tokens[i];
-            int num = Integer.parseInt(tokens[i + 1]);
+                int left = Integer.parseInt(tokens.get(i - 1));
+                int right = Integer.parseInt(tokens.get(i + 1));
+
+                int result = left * right;
+
+                tokens.set(i - 1, String.valueOf(result));
+                tokens.remove(i);
+                tokens.remove(i);
+            } else {
+                i++;
+            }
+        }
+
+        // 덧셈, 뺄셈 계산 처리
+        int result = Integer.parseInt(tokens.get(0)); // 첫 번째 숫자로 시작
+
+        for (i = 1; i < tokens.size(); i += 2) {
+            String operator = tokens.get(i); // 연산자 (+ 또는 -)
+            int num = Integer.parseInt(tokens.get(i + 1)); // 다음 숫자
 
             if (operator.equals("+")) {
                 result += num;
             } else if (operator.equals("-")) {
                 result -= num;
-            } else if (operator.equals("*")) {
-                result *= num;
             }
         }
+
         return result;
+
     }
 }
