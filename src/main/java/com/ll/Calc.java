804070466afc8439a87a3427becf0e762a18540a
HeungJunBag <p2005hihi@gmail.com>
2026-04-20 16:35:39 +0900

    feat: 뺄셈 연산 지원 (t5, t6, t7 통과)

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -2,6 +2,16 @@ package com.ll;
 
 public class Calc {
     public static int run(String expression) {
+
+        if (expression.contains(" - ")) {
+            String[] expressionBits = expression.split(" - ");
+
+            int num1 = Integer.parseInt(expressionBits[0]);
+            int num2 = Integer.parseInt(expressionBits[1]);
+
+            return num1 - num2;
+        }
+
         String[] expressionBits = expression.split(" \\+ ");
 
         int num1 = Integer.parseInt(expressionBits[0]);
@@ -9,5 +19,4 @@ public class Calc {
 
         return num1 + num2;
     }
-
 }
